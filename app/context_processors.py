a9d88255c6a681187d838f363d606745958208b4
Turner Halligan <turnerhusa@gmail.com>
2019-11-16 19:11:25 -0500

    Fixed error involving dog list

--- app/context_processors.py
+++ app/context_processors.py
@@ -2,7 +2,11 @@ from .models import UserProfile
 
 def add_variable_to_context(request):
     if (request.user is not None):
-        authUser = UserProfile.objects.get(pk=request.user.id)
-    return {
-        'authUserDogs': authUser.dogs.all()
-    }
+        try: 
+            authUser = UserProfile.objects.get(pk=request.user.id)
+            return {
+                'authUserDogs': authUser.dogs.all(),
+            }
+        except:
+            return {}
+    
