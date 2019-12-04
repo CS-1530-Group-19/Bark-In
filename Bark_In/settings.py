4dc1bb618a1a905c2f419a600f1e97d1e98685ce
Connor Kalina <cjk94@pitt.edu>
2019-12-03 19:10:01 -0500

    heroku

--- Bark_In/settings.py
+++ Bark_In/settings.py
@@ -131,3 +131,5 @@ STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 import dj_database_url 
 prod_db  =  dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(prod_db)
+
+WHITENOISE_USE_FINDERS = True
